610ba35eca6324c44c8f70d89d013a8bd869b9a0
yassinebk <yassinebk23@gmail.com>
2022-08-21 23:14:37 +0100

    sidebar bug

--- src/lib/header/Sidebar.svelte
+++ src/lib/header/Sidebar.svelte
@@ -43,56 +43,54 @@
 	</ul>
 	<div class="v-spacer-lg" />
 	<div class="buttons-section">
+		{#if $page.data.user}
+			<a class="btn" transition:fly={{ delay: 880, easing: circIn }} href="/api/auth">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="icon icon-tabler icon-tabler-phone-call"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					stroke-width="1"
+					stroke="#000000"
+					fill="none"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				>
+					<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+					<path
+						d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2"
+					/>
+					<path d="M15 7a2 2 0 0 1 2 2" />
+					<path d="M15 3a6 6 0 0 1 6 6" />
+				</svg>
 
-		{#if user}
-		<a class="btn" transition:fly={{ delay: 880, easing: circIn }} href="/api/auth">
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				class="icon icon-tabler icon-tabler-phone-call"
-				width="24"
-				height="24"
-				viewBox="0 0 24 24"
-				stroke-width="1"
-				stroke="#000000"
-				fill="none"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-			>
-				<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-				<path
-					d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2"
-				/>
-				<path d="M15 7a2 2 0 0 1 2 2" />
-				<path d="M15 3a6 6 0 0 1 6 6" />
-			</svg>
-
-			<p style="margin-left:6px;">Signin</p>
-		</a>
+				<p style="margin-left:6px;">Signin</p>
+			</a>
 		{:else}
+			<a class="btn" transition:fly={{ delay: 880, easing: circIn }} href="/api/signout">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="icon icon-tabler icon-tabler-phone-call"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					stroke-width="1"
+					stroke="#000000"
+					fill="none"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				>
+					<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+					<path
+						d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2"
+					/>
+					<path d="M15 7a2 2 0 0 1 2 2" />
+					<path d="M15 3a6 6 0 0 1 6 6" />
+				</svg>
 
-<a class="btn" transition:fly={{ delay: 880, easing: circIn }} href="/api/signout">
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				class="icon icon-tabler icon-tabler-phone-call"
-				width="24"
-				height="24"
-				viewBox="0 0 24 24"
-				stroke-width="1"
-				stroke="#000000"
-				fill="none"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-			>
-				<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-				<path
-					d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2"
-				/>
-				<path d="M15 7a2 2 0 0 1 2 2" />
-				<path d="M15 3a6 6 0 0 1 6 6" />
-			</svg>
-
-			<p style="margin-left:6px;">Signout</p>
-		</a>
+				<p style="margin-left:6px;">Signout</p>
+			</a>
 		{/if}
 		<button class="btn" transition:fly={{ delay: 880, easing: circIn }}>
 			<svg
