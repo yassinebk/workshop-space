f41dd1b5ffa684076274de488c3024288bfe8beb
yassinebk <yassinebk23@gmail.com>
2022-08-21 15:45:54 +0100

    fixed

--- src/lib/footer/Footer.svelte
+++ src/lib/footer/Footer.svelte
@@ -1,5 +1,95 @@
 <script lang="ts"></script>
 
+<div class="v-section-spacer-md" />
 <footer>
-	<div />
+	<ul class="navs">
+		<li><a href="/">Home</a></li>
+		<li><a href="/workshops">Workshops </a></li>
+		<li><a href="/">FAQ </a></li>
+		<li><a href="/ressources">Ressources </a></li>
+	</ul>
+	<div class="v-spacer-md" />
+	<div class="my-part">
+		<p>Buit With 🧡 from <a href="http://yassinebelkhadem.ninja">@F1r3creCk3r5</a></p>
+		<a href="/twitter"
+			><svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="icon icon-tabler icon-tabler-brand-twitter"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="#ff4500"
+				fill="none"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			>
+				<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+				<path
+					d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c-.002 -.249 1.51 -2.772 1.818 -4.013z"
+				/>
+			</svg></a
+		>
+
+		<a href="https://www.linkedin.com/in/yassine-belkhadem-396266204/"
+			><svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="icon icon-tabler icon-tabler-brand-linkedin"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="#ff4500"
+				fill="none"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			>
+				<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+				<rect x="4" y="4" width="16" height="16" rx="2" />
+				<line x1="8" y1="11" x2="8" y2="16" />
+				<line x1="8" y1="8" x2="8" y2="8.01" />
+				<line x1="12" y1="16" x2="12" y2="11" />
+				<path d="M16 16v-3a2 2 0 0 0 -4 0" />
+			</svg></a
+		>
+	</div>
 </footer>
+
+<style>
+	.navs {
+		display: flex;
+		width: 30%;
+		justify-content: space-evenly;
+	}
+	.navs li a {
+		transition: all 300ms ease-in-out;
+	}
+	.navs li:hover a {
+		color: var(--secondary);
+	}
+	footer {
+		padding: 2rem 2rem;
+		widows: 100vw;
+		background-color: #242424;
+	}
+	.my-part {
+		display: flex;
+		font-weight: bold;
+		margin: auto auto;
+		width: fit-content;
+	}
+
+	.my-part a {
+		color: #ff4500;
+		margin: 0px 0.2rem;
+	}
+
+	.my-part a:hover {
+		opacity: 0.8;
+	}
+
+	.my-part a:hover svg {
+		fill: #ff4500;
+		opacity: 0.8;
+	}
+</style>
---
+++ src/routes/session/[slug]/+page.ts
@@ -0,0 +1,49 @@
+import { initializeApiClient } from '$lib/sanity/client';
+import type { PageLoad } from '.svelte-kit/types/src/routes/$types';
+import imageUrlBuilder from '@sanity/image-url';
+
+export const load: PageLoad = async ({ params, fetch, data, depends, parent }) => {
+	console.log(params, fetch, data, depends);
+	const { user } = await parent();
+
+	const { slug } = params;
+
+	function urlFor(source: string) {
+		return builder.image(source);
+	}
+
+	const sanity = initializeApiClient();
+	const builder = imageUrlBuilder(sanity);
+	type view = {
+		_ref: string;
+		_id: string;
+		to: string;
+	};
+	let workshop;
+	workshop = (await sanity.fetch(`*[_type=="workshop_session" && slug.current=="${slug}"]`))[0];
+
+	workshop.main_image = urlFor(workshop.display_image);
+	console.log(user._id);
+	console.log(workshop);
+
+	if (!workshop.views.map((v: any) => v._id).includes(user._id)) {
+		console.log('here');
+		workshop = await sanity
+			.patch(workshop._id)
+			.setIfMissing({ views: [] })
+			.append('views', [{ _id: user._id, _type: 'reference' }])
+			.commit({ autoGenerateArrayKeys: true });
+
+		console.log(workshop);
+	}
+
+	// Check for the sesison !
+	// Check if the user is confirmed
+	// Check if the param is present
+	// Fetch data and feed it to the page
+
+	return {
+		workshop,
+		user
+	};
+};
